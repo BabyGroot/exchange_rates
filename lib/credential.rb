5166967c37452d1840dfa9bc19a051d9385858db
Martin <it3rnity@gmail.com>
2019-09-11 11:29:23 +0200

    Use env variables for credentials

--- lib/credential.rb
+++ lib/credential.rb
@@ -1,9 +1,9 @@
 class Credential
   def self.google_client_id
-    Rails.application.credentials.google[:client_id]
+    ENV['GOOGLE_AUTH_CLIENT_ID'] || Rails.application.credentials.google[:client_id]
   end
 
   def self.google_client_secret
-    Rails.application.credentials.google[:client_secret]
+    ENV['GOOGLE_AUTH_CLIENT_SECRET'] || Rails.application.credentials.google[:client_secret]
   end
 end
